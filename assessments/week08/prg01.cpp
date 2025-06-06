70d6a64bf2b8dd88910616c2cc296de1acef9b5b
manu-sankar <manusankar0601@gmail.com>
2025-06-06 18:02:33 +0530

    updated

--- assessments/week08/prg01.cpp
+++ assessments/week08/prg01.cpp
@@ -1,112 +1,177 @@
-#include<iostream>
-#include<vector>
-#include<set>
-#include<fstream>
-#include<string>
-#include<unordered_map>
-#include<set>
-
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <unordered_map>
 
 using namespace std;
 
-//Logger
-class Logger
-{
-private:
-	ofstream outFile;
+class Memory {
 public:
-	void writeInstr()
-	{
-		if (outFile.is_open())
-		{
-			try
-			{
-				if (outFile.is_open())
-				{
-					throw "File already opened";
-				}
-			}
-			catch (string s)
-			{
-				cout << s << endl;
-				exit(0);
-			}
-		}
-	}
+    Memory() {
+        memory.resize(65536, 0);
+    }
+
+    int read(int address) {
+        return memory[address];
+    }
+
+    void write(int address, int value) {
+        memory[address] = value;
+    }
+
+    void printSnapshot() {
+        for (int i = 0; i < 16; ++i) {
+            cout << i << " -> " << memory[i] << endl;
+        }
+    }
+
+private:
+    vector<int> memory;
 };
 
-class Processor
-{
-private:
-	vector<int>memory;
-	unordered_map<string, int>registers;
-	int prgcounter=0;
-	int memoryaddr=0;
-	bool isHalted = false;
+class Registers {
 public:
+    Registers() {
+        registers["AX"] = 0;
+        registers["BX"] = 0;
+        registers["CX"] = 0;
+        registers["DX"] = 0;
+    }
 
+    int get(const string& reg) {
+        return registers[reg];
+    }
 
-	Processor()
-	{
-		registers["AX"] = 0;
-		registers["BX"] = 0;
-		registers["CX"] = 0;
-		registers["DX"] = 0;
-	}
-	
-	//Load file
-	void load()
-	{
-		vector<string>instr;
-		ifstream fileIn;
-		try
-		{
-			
-			fileIn.open("instruction.txt");
-			if (!fileIn.is_open())
-			{
-				throw("Failed to open");
-			}
-		}
-		catch (string s)
-		{
-			cout << s << endl;
-		}
-		string line;
-		while (getline(fileIn,line ))
-		{
-			if (!line.empty())
-			{
-				instr.push_back(line);
-			}
-		}
-		
-	}
+    void set(const string& reg, int value) {
+        registers[reg] = value;
+    }
 
-	int getValue(const string& op)
-	{
-		if (op.empty() && op.front() == '[' && op.back() == ']')
-		{
-			int adr = stoi(op.substr(1, op.size() - 2));
-			return memoryaddr;
-		}
-		else if (registers.count(op))
-		{
-			return registers[op];
-		}
-		else
-		{
-			return stoi(op);
-		}
-	}	
+    void disp() {
+        cout << "AX: " << registers["AX"] << endl;
+        cout << "BX: " << registers["BX"] << endl;
+        cout << "CX: " << registers["CX"] << endl;
+        cout << "DX: " << registers["DX"] << endl;
+    }
+
+private:
+    unordered_map<string, int> registers;
 };
 
-int setValue(const string& str, int val)
-{
+class Processor {
+public:
+    Processor() : pc(0), running(true) {}
 
-	return val;
+    void loadInstructions(const vector<string>& instructions) {
+        this->instructions = instructions;
+    }
+
+    void execute(Memory& memory, Registers& registers) {
+        while (running && pc < instructions.size()) {
+            string instruction = instructions[pc];
+            parseAndExecute(instruction, memory, registers);
+            pc++;
+        }
+    }
+
+    void printSnapshot(Memory& memory, Registers& registers) {
+        cout << "Received Interrupt" << endl;
+        registers.disp();
+        memory.printSnapshot();
+    }
+
+private:
+    int pc;
+    bool running;
+    vector<string> instructions;
+
+    void parseAndExecute(const string& instruction, Memory& memory, Registers& registers) {
+        size_t firstSpace = instruction.find(' ');
+        if (firstSpace == string::npos) {
+            if (instruction == "HLT") running = false;
+            return;
+        }
+
+        string op = instruction.substr(0, firstSpace);
+        string args = instruction.substr(firstSpace + 1);
+
+        if (op == "MOV") {
+            handleMOV(args, memory, registers);
+        }
+        else if (op == "ADD") {
+            handleADD(args, registers);
+        }
+        else if (op == "SUB") {
+            handleSUB(args, registers);
+        }
+    }
+
+    void handleMOV(string args, Memory& memory, Registers& registers) {
+        size_t spacePos = args.find(' ');
+        string dest = args.substr(0, spacePos);
+        string src = args.substr(spacePos + 1);
+
+        if (src[0] == '[') { // Memory address
+            int address = stoi(src.substr(1, src.size() - 2));
+            if (dest[0] == '[') { // Memory to Memory
+                int srcValue = memory.read(address);
+                int destAddress = stoi(dest.substr(1, dest.size() - 2));
+                memory.write(destAddress, srcValue);
+            }
+            else { // Register to Memory
+                memory.write(stoi(dest.substr(1, dest.size() - 2)), registers.get(src));
+            }
+        }
+        else if (isdigit(src[0]) || src[0] == '-') { // Immediate value
+            if (dest[0] == '[') { // Memory
+                memory.write(stoi(dest.substr(1, dest.size() - 2)), stoi(src));
+            }
+            else { // Register
+                registers.set(dest, stoi(src));
+            }
+        }
+        else { // Register to Register
+            registers.set(dest, registers.get(src));
+        }
+    }
+
+    void handleADD(string args, Registers& registers) {
+        size_t spacePos = args.find(' ');
+        string dest = args.substr(0, spacePos);
+        string src = args.substr(spacePos + 1);
+        int value = (isdigit(src[0]) || src[0] == '-') ? stoi(src) : registers.get(src);
+        registers.set("AX", registers.get(dest) + value);
+    }
+
+    void handleSUB(string args, Registers& registers) {
+        size_t spacePos = args.find(' ');
+        string dest = args.substr(0, spacePos);
+        string src = args.substr(spacePos + 1);
+        int value = (isdigit(src[0]) || src[0] == '-') ? stoi(src) : registers.get(src);
+        registers.set("AX", registers.get(dest) - value);
+    }
+};
+
+int main() {
+    Memory memory;
+    Registers registers;
+    Processor processor;
+
+    ifstream infile("instructions.txt");
+    vector<string> instructions;
+    string line;
+
+    while (getline(infile, line)) {
+        if (line == "HLT") {
+            instructions.push_back(line);
+            break;
+        }
+        instructions.push_back(line);
+    }
+
+    processor.loadInstructions(instructions);
+    processor.execute(memory, registers);
+    processor.printSnapshot(memory, registers);
+
+    return 0;
 }
-
-
-
-
